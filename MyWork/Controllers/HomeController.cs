be6a7d5e8cd0ded5d0872479b39bd1e72c21c7cb
6254 <6254@TV6254NB>
2017-05-21 04:42:47 +0800

    客戶登入,登出

--- MyWork/Controllers/HomeController.cs
+++ MyWork/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MyWork.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MyWork.Controllers
 {
@@ -26,5 +28,46 @@ namespace MyWork.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginVM form)
+        {
+            if (checkLogin(form)) {
+
+                System.Web.Security.FormsAuthentication.RedirectFromLoginPage(form.UserName, false);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+    private bool checkLogin(LoginVM form)
+    {
+        if (form.UserName == "admin")
+        {
+            if (form.Password == "123")
+            {
+                return true;
+            }
+        } else{
+            var repo = RepositoryHelper.Get客戶資料Repository();
+            var curPW = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(form.Password, "SHA1");
+
+                return repo.All().Any(p => p.帳號 == form.UserName && p.密碼 == curPW);
+         }
+        return false;
+      }
+    
     }
 }
---
+++ MyWork/Models/LoginVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWork.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
